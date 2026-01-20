3eae1d4a304b52cfa24099fc8e40428ffc0547fc
Rafael Moura <87615082+RafaelMoura11@users.noreply.github.com>
2026-01-20 10:58:54 -0300

    Removing welcome blade file and creating a simple blade to show at route /main

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,5 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MainController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Route::get('/main', [MainController::class, 'index']);
 
