4b2c60c8c5ed74d8c3b4d1f101a2b5ab0136c9a4
Rafael Moura <87615082+RafaelMoura11@users.noreply.github.com>
2026-01-21 09:37:23 -0300

    Creating main layout and extending it through different pages

--- routes/web.php
+++ routes/web.php
@@ -4,4 +4,6 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MainController;
 
 Route::get('/main/{value}', [MainController::class, 'index']);
+Route::get('/page2/{value}', [MainController::class, 'page2']);
+Route::get('/page3/{value}', [MainController::class, 'page3']);
 
