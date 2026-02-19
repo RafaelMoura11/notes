bcf36b47caf6015ee1f517c3cee279cc6595187a
Rafael Moura <87615082+RafaelMoura11@users.noreply.github.com>
2026-02-19 08:58:19 -0300

    Redirect user from route / to /login

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\AuthController;
 use App\Http\Controllers\MainController;
 use Illuminate\Support\Facades\Route;
 
+Route::redirect('/', '/login');
 Route::get('/login', [AuthController::class, 'login']);
 Route::post('loginSubmit', [AuthController::class,'loginSubmit']);
 Route::get('/logout', [AuthController::class, 'logout']);
